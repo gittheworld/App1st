f5cbee21a68fbbc72fb794499c38c21471ac7c85
Danny Song <hsinchu.kong@gmail.com>
2020-07-31 22:16:46 +0800

    Initial Commit

---
+++ App1st/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App1st
+//
+//  Created by danny on 2020/7/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
